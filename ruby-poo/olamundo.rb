a9b63acab66d17bf8dbcacf1d3f62d20958d156a
Derek Barragão <derekbarragao@gmail.com>
2024-04-21 21:46:43 -0300

    novas aulas
    adição do arquivo variáveis e comentários explicando cada coisa explicada até o momento

--- ruby-poo/olamundo.rb
+++ ruby-poo/olamundo.rb
@@ -1,12 +1,17 @@
+#para printar uma string utilizar puts
 puts "ola mundo"
+#professor falou que vai explicar mais para frente o porque ele utilizou "#{ + }"
 puts "#{1+2}"
-
+#utilizar .class para ver o tipo
 puts 23.class
 puts "olamundo".class
 puts 20.3.class
+#aspas simples também funcionam como string
 puts '@'.class
+#dessa forma voce pode concatenar strings, quando um numero esta entre aspas ele se torna da classe string
 puts '3' + '1'
 puts '3' + "1"
+#também existe a classe bolean que no caso é trueclass ou falseclass
 puts true.class
 puts false.class
 
--- ruby-poo/variaveis.rb
+++ ruby-poo/variaveis.rb
@@ -1,7 +1,25 @@
+#atribuir variavel:
 v1 = 10
 v2 = 30
+#printar variaveis, resultados etc utilizar puts
 puts v1
+#usar .class para mostrar a classe (string, integer, float, TrueClass, FalseClass)
 puts v1.class
-
 puts v3 = v1 + v2
 puts v3.class
+
+#atribuindo uma variavel para que o usuario digite utilizando "gets"
+nome = gets
+#aqui usei .chomp para remover uma quebra de linha que aparece quando o usuario digita no gets
+puts "o seu nome é " + nome.chomp
+
+puts "digite sua idade:"
+#voce pode utilizar .chomp direto no gets que coleta o dado do usuario
+idade = gets.chomp
+puts "sua idade é: " + idade
+
+=begin
+voce pode comentar linha a linha utilizando # ou voce pode utilizar "=begin" para iniciar uma linha de comentario e no final "=end".
+puts "legal"
+puts "daora"
+=end
