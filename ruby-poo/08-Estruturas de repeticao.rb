bfe9020edf1c0b667f0a85893f53182bf49ec46f
Derek Barragão <derekbarragao@gmail.com>
2024-04-22 23:35:59 -0300

    finalizado Estrutura de repeticao

--- ruby-poo/08-Estruturas de repeticao.rb
+++ ruby-poo/08-Estruturas de repeticao.rb
@@ -1,2 +1,21 @@
 #Estruturas de repetição são utilizadas para fazer looping, pode utilizar while e until
 
+i = 1
+#enquanto isso for verdadeiro
+while i <= 50
+    puts "derek - #{i}"
+    i += 1
+end
+
+
+#enquanto for falso
+until i <= 0
+    puts "sou falso - #{i}"
+    i -= 1
+end
+
+puts "fim"
+
+
+
+
