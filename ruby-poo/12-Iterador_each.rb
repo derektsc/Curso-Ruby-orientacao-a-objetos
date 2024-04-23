52228e5273b35d88795399a66e37928fd59ca9fa
Derek Barragão <derekbarragao@gmail.com>
2024-04-23 13:35:02 -0300

    atualização dos comentarios do arquivo each

--- ruby-poo/12-Iterador_each.rb
+++ ruby-poo/12-Iterador_each.rb
@@ -1,37 +1,28 @@
 #each em ingles quer dizer cada
 #a ideia dele é quando você tem uma coleção (array, hashe) e você quer passar em cada um dos elementos fazendo alguma coisa
 
-=begin
-
-executar no irb:
+#executar no irb:
 
 a = [1,2,3,4]
 #vamos passar em cada um deles e tornar eles o dobro do seu valor utilizando each
-a.each { |elemento| puts elemento}
+a.each {|elemento| puts elemento}
 
 #se tiver mais de uma linha pode utilizar o do ao inves de {}
-
 a.each do |el|
     puts el
     puts el * 2
     puts el * 3
 end
-
-resultado:
-
+#resultado:
 1 #resultado do puts el 1
 2 #resultado do puts el 1 * 2
 3 #resultado do puts el 1 * 3
 2 #resultado do puts el 2
 4 #resultado do puts el 2 * 2
 6 #resultado do puts el 2 * 3
-3 ""
-6 ""
-9 ""
-4 ""
-8 ""
-12 ""
-
-
-
-=end
+3 #""
+6 #""
+9 #""
+4 #""
+8 #""
+12 #""
