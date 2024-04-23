24357346584c20461f314a9aff290a51728735f4
Derek Barragão <derekbarragao@gmail.com>
2024-04-22 23:43:53 -0300

    finalizar vetores e arrays

---
+++ ruby-poo/09-vetores_e_arrays.rb
@@ -0,0 +1,10 @@
+#Vetor voce reserva varios espaços de memorias ao mesmo tempo, guardando informações diferentes em cada espaço
+
+vetor = []
+
+#utilize .push para preencher seu vetor
+vetor.push 50
+#utilize .delete para deletar
+vetor.delete 50
+
+puts vetor
