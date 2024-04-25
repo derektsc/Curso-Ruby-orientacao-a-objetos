2bde7c63d3ecfa1b4520d72456f40e091211bcff
Derek Barragão <derekbarragao@gmail.com>
2024-04-24 22:36:35 -0300

    Criação do arquivo definindo atributos, nessa aula aprendemos a diferença dos atributos - e +, o que é uma variavel de instancia e como utiliza-la

---
+++ ruby_programacao_orientada_objetos/07-Definindo_Atributos.rb
@@ -0,0 +1,86 @@
+#Vamos aprender a Definir atributos
+=begin Diferença de atributos "-" e "+"
+    Pessoa
+        -nome: string
+        -idade: integer
+        +gritar()
+        +agradecer()
+    end
+
+    Atributos com o "-" na frente significa que eles são privados, ou seja eu não consigo acessar do meu objeto essas 2 propriedades, pois através do encapsulamento devemos ter um método que vá colocar os dados do atributo e um outro método que vai buscar os dados do primeiro
+
+    Atributos com o "+" na frente siginifica que eles são públicos, ou seja são métodos que irão buscar os dados dos atributos "-" armazenar e mostrar ao usuario
+
+    Pensando dessa forma a Classe pessoa deveria ser da seguinte forma:
+    Pessoa
+        -nome: string
+        -idade: integer
+        +gritar()
+        +agradecer()
+        +guardar_nome(nome)
+        +mostrar_nome()
+        +guardar_idade(idade)
+        +mostrar_idade()
+    end
+=end
+
+=begin variavel de instância
+    Quando fazemos isso: o @ significa que a variavel é uma variavel de instancia
+    @variavel
+    Estamos indicando para o ruby que essa variavel tera um valor para cada objeto.
+
+    Vejamos na pratica abaixo
+=end
+
+class Pessoa
+    @nome = nil #isso é uma variavel de instancia, declarei ela vazia aqui
+    @idade = nil
+
+    def guardar_nome(nome)
+        @nome = nome #armazenando o nome dentro da variavel de instancia "nome"
+    end
+
+    def mostrar_nome
+        @nome
+    end
+
+    def guardar_idade(idade)
+        @idade = idade
+    end
+
+    def mostrar_idade
+        @idade
+    end
+
+    def gritar(gritando = "AAAA")
+        puts gritando
+    end
+
+    def agradecer(agradecendo = "obrigado")
+        puts agradecendo
+    end
+end
+
+
+pessoa1 = Pessoa.new
+pessoa1.guardar_nome("Derek")
+pessoa1.guardar_idade(23)
+
+pessoa2 = Pessoa.new
+pessoa2.guardar_nome("Matheus")
+pessoa2.guardar_idade(26)
+
+puts pessoa1.mostrar_nome
+puts pessoa1.mostrar_idade
+
+puts pessoa2.mostrar_nome
+puts pessoa2.mostrar_idade
+
+
+
+fala_pessoa = Pessoa.new
+puts fala_pessoa.gritar("EU NÃO QUERO MAIS ISSO!!")
+puts fala_pessoa.agradecer
+
+
+
