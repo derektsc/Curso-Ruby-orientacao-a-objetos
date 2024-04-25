8cf780c48726d3ab0b364d2f8379b29ac493d7d7
Derek Barragão <derekbarragao@gmail.com>
2024-04-25 00:09:22 -0300

    Finalizando definição de atributo, porém vou criar um documento 07.2 para explicar o attr

--- ruby_programacao_orientada_objetos/07-Definindo_Atributos.rb
+++ ruby_programacao_orientada_objetos/07-Definindo_Atributos.rb
@@ -36,19 +36,19 @@ class Pessoa
     @nome = nil #isso é uma variavel de instancia, declarei ela vazia aqui
     @idade = nil
 
-    def guardar_nome(nome)
+    def guardar_nome(nome) #convercao do ruby voce poderia utilizar nome=(nome)
         @nome = nome #armazenando o nome dentro da variavel de instancia "nome"
     end
 
-    def mostrar_nome
+    def mostrar_nome   #convencao do ruby voce poderia utilizar apenas nome
         @nome
     end
 
-    def guardar_idade(idade)
+    def set_idade(idade) #no lugar de guardar e mostrar podemos utilizar get/set, isso vem do java
         @idade = idade
     end
 
-    def mostrar_idade
+    def get_idade
         @idade
     end
 
@@ -64,17 +64,17 @@ end
 
 pessoa1 = Pessoa.new
 pessoa1.guardar_nome("Derek")
-pessoa1.guardar_idade(23)
+pessoa1.set_idade(23)
 
 pessoa2 = Pessoa.new
 pessoa2.guardar_nome("Matheus")
-pessoa2.guardar_idade(26)
+pessoa2.set_idade(26)
 
 puts pessoa1.mostrar_nome
-puts pessoa1.mostrar_idade
+puts pessoa1.get_idade
 
 puts pessoa2.mostrar_nome
-puts pessoa2.mostrar_idade
+puts pessoa2.get_idade
 
 
 
@@ -83,4 +83,18 @@ puts fala_pessoa.gritar("EU NÃO QUERO MAIS ISSO!!")
 puts fala_pessoa.agradecer
 
 
+#FORMATO RUBY PADRÃO
+class Teste
+    @nome = nil #variavel de instancia
+    def nome=(nome)
+        @nome = nome
+    end
+    def nome
+        @nome
+    end
+end
+
+nome1 = Teste.new
+nome1.nome = "sharon"
+puts nome1.nome
 
