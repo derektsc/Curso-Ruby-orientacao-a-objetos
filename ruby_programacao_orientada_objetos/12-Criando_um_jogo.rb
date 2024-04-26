9cc5fab489a7a23564e66e9a97163911f84e2dc9
Derek Barragão <derekbarragao@gmail.com>
2024-04-25 22:49:40 -0300

    Exercicio criando um jogo de advinhar, usamos a classe Random com o método rand para a maquina sortear um número aleatorio e o usuario tentar adivinhar qual é esse número

---
+++ ruby_programacao_orientada_objetos/12-Criando_um_jogo.rb
@@ -0,0 +1,41 @@
+=begin Descrição da aula + explicação do random.rand
+    Vamos criar um jogo que adivinha o número que o computador vai sortear
+    Random.rand é uma classe que contém um método chamado rand, veja como utilizar no link abaixo, também vamos usar nesse exercicio.
+    https://ruby-doc.org/core-2.4.0/Random.html#method-i-rand
+=end
+
+class AdivinharNumero
+    attr_reader :numero
+    attr_reader :venceu
+
+    def initialize
+        @numero = Random.rand(10)
+        @venceu = false
+    end
+
+    def tentar_advinhar(numero)
+        if numero == @numero
+            @venceu = true
+            "Você venceu!!" #poderia colocar puts ou return antes mas também funciona sem nada 
+        elsif numero > @numero
+            "O numero digitado é maior"
+        else
+            "O numero digitado é menor"
+        end
+    end
+end
+
+jogo = AdivinharNumero.new
+
+
+puts "Digite um número"
+numero = gets.to_i
+puts jogo.tentar_advinhar(numero)
+
+until jogo.venceu do
+puts "Digite outro número"
+numero = gets.to_i
+puts jogo.tentar_advinhar(numero)
+end
+
+
